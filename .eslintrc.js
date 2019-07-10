a9ca95634f23101536af6ae311dcee57fd26bc53
Radha Kancharla <radha.kancharla@ncfbins.com>
2019-07-10 12:19:59 -0400

    first Commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  // extends: [
+  //   'prettier',
+  //   'prettier/vue',
+  //   'plugin:prettier/recommended',
+  //   '@nuxtjs',
+  //   'plugin:nuxt/recommended'
+  // ],
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'prettier',
+    'prettier/vue',
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {}
+}
